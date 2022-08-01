d9b5fcc45a1bd01f8e213d1ab7cbba03adea3ae9
itsharsh334 <80212334+itsharsh334@users.noreply.github.com>
2022-08-01 20:59:24 +0530

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,12 @@
+var randomNumber1=Math.floor(Math.random()*6)+1
+var str1="images/dice"+randomNumber1+".png";
+document.querySelector(".img1").setAttribute("src",str1);
+var randomNumber2=Math.floor(Math.random()*6)+1
+var str2="images/dice"+randomNumber2+".png";
+document.querySelector(".img2").setAttribute("src",str2);
+if(randomNumber1>randomNumber2)
+  document.querySelector("h1").innerHTML="🚩Player 1 Wins";
+else if (randomNumber1<randomNumber2)
+  document.querySelector("h1").innerHTML="Player 2 Wins🚩";
+else
+  document.querySelector("h1").innerHTML="It's a Draw";
